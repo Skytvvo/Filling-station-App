368d524339f4d24b1a5308f65deb59e54fe270f5
Ilya Kazakou <pouutewqer@gmail.com>
2021-04-03 00:43:56 +0300

    Modified support page

---
+++ FogOilAssistant/Components/Data/Pages/Support/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FogOilAssistant.Components.Data.Pages.Support
+{
+    public class SupportItem
+    {
+        string text;
+        string name;
+        string color;
+        string link;
+
+        public string Text { get => text; set => text = value; }
+        public string Name { get => name; set => name = value; }
+        public string Color { get => color; set => color = value; }
+        public string Link { get => link; set => link = value; }
+    }
+}
--- FogOilAssistant/Components/Models/Pages/ViewModelSupport.cs
+++ FogOilAssistant/Components/Models/Pages/ViewModelSupport.cs
@@ -1,13 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using FogOilAssistant.Components.Data.Pages.Support;
 
 namespace FogOilAssistant.Components.Models.Pages
 {
-    public class ViewModelSupport
+    public class ViewModelSupport : INotifyPropertyChanged
     {
+        List<SupportItem> supportItems;
 
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public List<SupportItem> SupportItems { get => supportItems;
+            set {
+                this.supportItems = value;
+                OnPropertyChanged("SupportItems");
+            }
+        }
+
+        public ViewModelSupport()
+        {
+            this.SupportItems = new List<SupportItem>()
+            {
+                new SupportItem(){ Color="#E31E8F", Link="#", Name="Feedback", Text="Your feedback will be processe by our technical support."},
+                new SupportItem(){Color="#00E5FF", Link="#", Name="Consultant", Text="Online consultation with our employee"}
+               
+            };
+        }
     }
 }
