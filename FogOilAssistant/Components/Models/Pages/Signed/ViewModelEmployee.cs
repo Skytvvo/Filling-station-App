3af04c38d34aaa63b1dbc697bddd1b1aa2d61e23
Ilya Kazakou <pouutewqer@gmail.com>
2021-04-27 18:34:09 +0300

    Implement feature for usercontrol => parent can send cammand params to child (usercontrol)

---
+++ FogOilAssistant/Components/Models/Pages/Signed/ViewModelEmployee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using FogOilAssistant.Components.Data;
+
+namespace FogOilAssistant.Components.Models.Pages.Signed
+{
+    
+    public class ViewModelEmployee: INotifyPropertyChanged
+    {
+        private List<RelayCommand> commandList;
+
+        public List<RelayCommand> CommandList
+        {
+            get => commandList;
+            set
+            {
+                commandList = value;
+                OnPropertyChanged("CommandList");
+            }
+        }
+        public ViewModelEmployee()
+        {
+            CommandList = new List<RelayCommand>()
+            {
+                new RelayCommand((obj) =>
+                {
+                    MessageBox.Show("1");
+                }),
+                new RelayCommand((obj) =>
+                {
+                    MessageBox.Show("2");
+
+                }),
+                new RelayCommand((obj) =>
+                {
+                    MessageBox.Show("3");
+
+                })
+            };
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ FogOilAssistant/Components/View/Pages/EmployeeControls/Options.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FogOilAssistant.Components.Data;
+
+namespace FogOilAssistant.Components.View.Pages.EmployeeControls
+{
+    /// <summary>
+    /// Логика взаимодействия для Options.xaml
+    /// </summary>
+    public partial class Options : UserControl
+    {
+        public List<RelayCommand> ActionProp
+        {
+            get { return (this.GetValue(ActionPropProperty) as List<RelayCommand>); }
+            set { this.SetValue(ActionPropProperty, value); }
+        }
+
+        public static DependencyProperty ActionPropProperty = DependencyProperty.Register(
+          "ActionProp", typeof(List<RelayCommand>), typeof(Options), new PropertyMetadata(null));
+
+
+        public Options()
+        {
+            InitializeComponent();
+        }
+
+       
+        
+    }
+}
